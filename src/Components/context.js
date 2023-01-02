432c57e21be4f5c2c76377493ea62a0fba633d15
Samonto Karmaker <samonto0726@gmail.com>
2023-01-02 20:49:05 +0600

    Fetching all recipes using fetch api

--- src/Components/context.js
+++ src/Components/context.js
@@ -1,8 +1,26 @@
-import React, {useContext} from "react";
+import React, {useContext, useEffect, useState} from "react";
 
 const AppContext = React.createContext()
 
+const allRecipes = "https://www.themealdb.com/api/json/v1/1/search.php?s="
+const randomRecipe = "https://www.themealdb.com/api/json/v1/1/random.php"
+
 const AppProvider = ({children}) => {
+    const fetchData = async url => {
+        try{
+            const response = await fetch(url)
+            const data = await response.json()
+            console.log(data)
+        }
+        catch (error){
+            console.log(error)
+        }
+    }
+
+    useEffect(() => {
+        fetchData(allRecipes)
+    }, [])
+
     return(
         <AppContext.Provider value="Hello World">
             {children}

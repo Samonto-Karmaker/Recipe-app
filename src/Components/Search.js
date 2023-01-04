5d357bc55324c20303531850d07520f2d03d7608
Samonto Karmaker <samonto0726@gmail.com>
2023-01-04 20:57:18 +0600

    Complete Search UI

--- src/Components/Search.js
+++ src/Components/Search.js
@@ -1,9 +1,35 @@
-import React from "react";
+import React, {useState, useContext} from "react";
+import "../App.css"
 
 const Search = () => {
+
+    const [search, setSearch] = useState("")
+
+    const handleInputChange = event => {
+        setSearch(event.target.value)
+    }
+
+    const handleSearchSubmit = event => {
+        event.preventDefault()
+    }
+
     return(
-        <div>
-            <h1>Search</h1>
+        <div className="search-container">
+            <form className="search-form" onSubmit={handleSearchSubmit}>
+                <input 
+                    type="text" 
+                    placeholder="Search Recipes"
+                    className="form-input" 
+                    value={search}
+                    onChange={handleInputChange}
+                />
+                <button type="submit"className="btn">
+                    Search
+                </button>
+                <button type="button"className="btn btn-hipster">
+                    Suprise me!!
+                </button>
+            </form>
         </div>
     )
 }
